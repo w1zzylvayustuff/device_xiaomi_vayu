38baaac5cbbc4cbf98111eaf93ed6f3c96dae00e
w1zzyl <w1zzyl@protonmail.com>
2022-04-23 22:26:57 +0200

    vayu: Use proton clang
    Signed-off-by: w1zzyl <w1zzyl@protonmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,6 +90,7 @@ TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_IMAGE_NAME := Image
 TARGET_KERNEL_CONFIG := vayu_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/proton
 TARGET_KERNEL_SOURCE := kernel/xiaomi/vayu
 TARGET_KERNEL_ADDITIONAL_FLAGS += LLVM=1
 
